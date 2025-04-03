5d6d36b768982dd523b28be7278fc2468a33593e
Adam Richard Turner <hello@adamrichardturner.dev>
2025-04-03 06:42:24 +0100

    further tweaks

--- app/root.tsx
+++ app/root.tsx
@@ -21,6 +21,10 @@ export const meta: MetaFunction = () => {
   return [
     { title },
     { name: 'description', content: description },
+    {
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1, maximum-scale=1',
+    },
 
     // Open Graph / Facebook
     { property: 'og:type', content: 'website' },
